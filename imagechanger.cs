8e284092aac6d2941049eec1aaf53cf5d0c642cb
noHero <warseppl@googlemail.com>
2013-06-28 03:08:06 +0200

    added background changer + mvd to after invoke
    added background changer + moved image changer invokes to afterinvoke

--- imagechanger.cs
+++ imagechanger.cs
@@ -25,6 +25,7 @@ namespace imagechanger.mod
         private string[] cardnames;
         private int[] cardImageid;
         private string[] imagefiles;
+        private string[] backgroundimages;
 		//initialize everything here, Game is loaded at this point
         public imagechanger()
 		{
@@ -34,9 +35,20 @@ namespace imagechanger.mod
             }
             catch { }
             this.imagefiles=Directory.GetFiles(this.ownpicpath, "*.png");
+            this.backgroundimages=Directory.GetFiles(this.ownpicpath, "background_*.png");
             
 		}
 
+        private string rndbackground() {
+            if (backgroundimages.Length > 0)
+            {
+                System.Random random = new System.Random();
+                int randomNumber = random.Next(0, backgroundimages.Length);
+
+                return backgroundimages[randomNumber];
+            }
+            else { return ""; }
+        }
 
 		public static string GetName ()
 		{
@@ -96,6 +108,7 @@ namespace imagechanger.mod
             {
                 return new MethodDefinition[] {
                     scrollsTypes["AssetLoader"].Methods.GetMethod("LoadTexture2D", new Type[]{typeof(string)}),
+                    scrollsTypes["BattleMode"].Methods.GetMethod("setupBackground", new Type[]{typeof(int)}),
                     scrollsTypes["CardView"].Methods.GetMethod("applyCardTexture")[0],
 
              };
@@ -106,8 +119,128 @@ namespace imagechanger.mod
             }
 		}
 
+        public override bool BeforeInvoke(InvocationInfo info, out object returnValue) {
+            if (info.targetMethod.Equals("setupBackground"))
+            {
+                int backGround = (int)info.arguments[0];
+                BackgroundData background = BackgroundData.getBackground(backGround);
+                BackgroundData currentBgData = (BackgroundData)typeof(BattleMode).GetField("currentBgData", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(info.target);
+                    
+                currentBgData = background;
+                Unit.shadowColor = background.shadowColor;
+                int num = -1;
+                while (true)
+                {
+                    Material material = new Material(Shader.Find("Transparent/Diffuse"));
+                    material.mainTextureOffset = new Vector2(0.01f, 0.01f);
+                    material.mainTextureScale = new Vector2(0.98f, 0.98f);
+                    WorldImage image = background.getImage(++num);
+                    if (image == null)
+                    {
+                        break;
+                    }
+                    GameObject gameObject = PrimitiveFactory.createPlane(false);
+                    string path = rndbackground();
+                    Texture2D mainTexture;
+                    if (path.Equals("")) {
+                         mainTexture = ResourceManager.LoadTexture(image.filename);
 
-        public override bool BeforeInvoke(InvocationInfo info, out object returnValue)
+
+                         GameObject lightSource = (GameObject)typeof(BattleMode).GetField("lightSource", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(info.target);
+                         GameObject lightSource2 = (GameObject)typeof(BattleMode).GetField("lightSource2", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(info.target);
+
+                         if (backGround == 1)
+                         {
+                             BattleMode lol = (BattleMode)info.target;
+                             GameObject gameObject2 = new GameObject();
+                             gameObject2.AddComponent<MeshRenderer>();
+                             gameObject2.AddComponent<EffectPlayer>();
+                             EffectPlayer component = gameObject2.GetComponent<EffectPlayer>();
+                             component.init("torch1", 1, lol, 50, new Vector3(0.5f, 0.5f, 0.5f), true, string.Empty, 0);
+                             gameObject2.transform.position = new Vector3(-3.4f, 0.75f, 5.565f);
+                             gameObject2.transform.eulerAngles = new Vector3(51f, 270f, 0f);
+                             GameObject gameObject3 = new GameObject();
+                             gameObject3.AddComponent<MeshRenderer>();
+                             gameObject3.AddComponent<EffectPlayer>();
+                             EffectPlayer component2 = gameObject3.GetComponent<EffectPlayer>();
+                             component2.init("torch1", 1, lol, 50, new Vector3(0.5f, 0.5f, 0.5f), true, string.Empty, 0);
+                             gameObject3.transform.position = new Vector3(-3.4f, 0.75f, -5.7225f);
+                             gameObject3.transform.eulerAngles = new Vector3(51f, 270f, 0f);
+
+                             lightSource = new GameObject("Light");
+                             lightSource.AddComponent<Light>();
+                             lightSource.light.color = Color.yellow;
+                             lightSource.light.cullingMask = ~(1 << BattleMode.LAYER_NOLIGHT);
+                             lightSource.transform.position = new Vector3(-2.86f, 0.75f, 5.37f);
+                             lightSource.light.intensity = 0.44f;
+                             lightSource.light.type = LightType.Point;
+                             lightSource.light.range = 10f;
+                             lightSource.light.shadows = LightShadows.Soft;
+                             lightSource2 = new GameObject("Light");
+                             lightSource2.AddComponent<Light>();
+                             lightSource2.light.color = Color.yellow;
+                             lightSource2.light.cullingMask = ~(1 << BattleMode.LAYER_NOLIGHT);
+                             lightSource2.transform.position = new Vector3(-2.86f, 0.75f, -5.48f);
+                             lightSource2.light.intensity = 0.44f;
+                             lightSource2.light.type = LightType.Point;
+                             lightSource2.light.range = 10f;
+                             lightSource2.light.shadows = LightShadows.Soft;
+                         }
+                         if (backGround == 4)
+                         {
+                             Color color = new Color(0.972549f, 0.5568628f, 0.219607845f);
+                             lightSource = new GameObject("Light");
+                             lightSource.AddComponent<Light>();
+                             lightSource.light.color = Color.yellow;
+                             lightSource.light.cullingMask = ~(1 << BattleMode.LAYER_NOLIGHT);
+                             lightSource.transform.position = new Vector3(-2.93f, 0.75f, 4.08f);
+                             lightSource.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                             lightSource.light.intensity = 0.44f;
+                             lightSource.light.type = LightType.Point;
+                             lightSource.light.range = 3f;
+                             lightSource.light.shadows = LightShadows.Soft;
+                             lightSource2 = new GameObject("Light");
+                             lightSource2.AddComponent<Light>();
+                             lightSource2.light.color = Color.yellow;
+                             lightSource2.light.cullingMask = ~(1 << BattleMode.LAYER_NOLIGHT);
+                             lightSource2.transform.position = new Vector3(-3.46f, 0.75f, -3.66f);
+                             lightSource2.light.intensity = 0.44f;
+                             lightSource2.light.type = LightType.Point;
+                             lightSource2.light.range = 3f;
+                             lightSource2.light.shadows = LightShadows.Soft;
+                         }
+                    }
+                    else
+                    {
+                         mainTexture = new Texture2D(2, 2, TextureFormat.RGB24, false);
+                        byte[] data = File.ReadAllBytes(path);
+                        mainTexture.LoadImage(data);
+                    }
+                    //
+                    
+                    material.renderQueue = 2 + num;
+                    gameObject.renderer.material = material;
+                    gameObject.renderer.material.mainTexture = mainTexture;
+                    gameObject.transform.position = image.pos;
+                    gameObject.transform.localScale = image.scale;
+                    gameObject.transform.eulerAngles = image.rot;
+                }
+                
+                Camera.main.transform.eulerAngles = new Vector3(51f, 270f, 0f);
+                Camera.main.transform.position = new Vector3(9.67f, 11.15f, 0f);
+
+
+                returnValue = null;
+                return true;
+            }
+
+            returnValue = null;
+            return false;
+        
+        }
+
+        public override void AfterInvoke (InvocationInfo info, ref object returnValue)
+        //public override bool BeforeInvoke(InvocationInfo info, out object returnValue)
         {
            
             if (info.targetMethod.Equals("LoadTexture2D"))
@@ -124,7 +257,7 @@ namespace imagechanger.mod
                         byte[] data = File.ReadAllBytes(this.ownpicpath + name + "_prev.png");
                         texture2D.LoadImage(data);
                         returnValue = texture2D;
-                        return true;
+                        return;//return true;
                     }
                     else
                     {
@@ -135,7 +268,7 @@ namespace imagechanger.mod
                             byte[] data = File.ReadAllBytes(this.ownpicpath + name + ".png");
                             texture2D.LoadImage(data);
                             returnValue = texture2D;
-                            return true;
+                            return;// return true;
                         }
                     }
                 }
@@ -169,8 +302,8 @@ namespace imagechanger.mod
                     }
                     Boolean hasSetHighResTexture = (Boolean)typeof(CardView).GetField("hasSetHighResTexture", BindingFlags.Instance | BindingFlags.NonPublic ).GetValue(info.target);
                     hasSetHighResTexture = true;
-                    returnValue = null;
-                    return true;
+                    //returnValue = null;
+                    return;//return true;
 
                 }
 
@@ -178,11 +311,11 @@ namespace imagechanger.mod
             }
          
 
-            returnValue = null;
-            return false;
+            //returnValue = null;
+            return;//return false;
         }
 
-		public override void AfterInvoke (InvocationInfo info, ref object returnValue)
+		/*public override void AfterInvoke (InvocationInfo info, ref object returnValue)
 		{
 
            
@@ -190,7 +323,7 @@ namespace imagechanger.mod
 
 
 			return;
-		}
+		}*/
 
 
 
